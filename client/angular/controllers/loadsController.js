840ac8d6b6a8dedd37376096408b9b87e00f0977
Kelvin Lu <kelvin.lu.sj@gmail.com>
2015-11-05 02:53:08 -0800

    basic testing for loads done excluding destroy

--- client/angular/controllers/loadsController.js
+++ client/angular/controllers/loadsController.js
@@ -1,11 +1,33 @@
-fleetLogistics.controller('loadsController', function($scope, loadsFactory){
+fleetLogistics.controller('loadsController', function($scope, loadsFactory, usersFactory, equipmentFactory){
 	
-  loadsFactory.index( function (data) {
-    $scope.loads = data;
-    console.log(data);
+  usersFactory.index( function (data) {
+    $scope.drivers = data;
   });
 
+  equipmentFactory.index( function (data) {
+    $scope.trucks = data.filter( function (equip) {
+      return equip.__t == "Truck";
+    });
+    $scope.trailers = data.filter( function (equip) {
+      return equip.__t == "Trailer";
+    });
+  });
+
+  loadsFactory.index( function (data) {
+    $scope.loads = data;
+  });
+
+  $scope.newLoad = {
+    _drivers: [],
+    _trucks: [],
+    _trailers: [],
+    pickup_numbers: [],
+    pickups: [],
+    dropoffs: []
+  }
+
   $scope.create = function() {
+    console.log($scope.newLoad);
     loadsFactory.create($scope.newLoad, function (data) {
       $scope.loads.push(data);
       $scope.load = data;
--- server/controllers/loads.js
+++ server/controllers/loads.js
@@ -1,10 +1,17 @@
 var mongoose = require('mongoose');
 var Load = mongoose.model('Load');
 var User = mongoose.model('User');
+var async = require('async');
+
+var save = function (doc, callback) {
+  doc.save(function (error, doc) {
+    callback(error, doc);
+  })
+};
 
 module.exports = {
   index: function (req, res) {
-    Load.find({}, function (err, results) {
+    Load.find({}).populate('_drivers _trucks _trailers').exec( function (err, results) {
       if (err) {
         console.log(err);
       }
@@ -21,25 +28,20 @@ module.exports = {
     load.paid = false;
     load.save( function (err) {
       // find all drivers for this load
-      User.find({_id: { $in: req.body.driver_ids} }, function (err, drivers) {
+      User.find({_id: { $in: req.body._drivers} }, function (err, drivers) {
+        // add this load to each driver
         for (var i = 0; i < drivers.length; i++) {
           drivers[i].loads.push(load);
-          // save this load to each driver
-          drivers[i].save( function (err) {
-            if (err) {
-              console.log(err);
-            }
-            console.log('driver saved');
-          });
         }
-        // process.nextTick( function () )
-        console.log('sending response');
-        res.json(load);
+        // save all drivers asynchronously
+        async.map(drivers, save, function (error) {
+          res.json(load);
+        });
       });      
     });
   },
   show: function (req, res) {
-    Load.findById(req.params.id, function (err, load) {
+    Load.findById(req.params.id).populate('_drivers _trucks _trailers').exec( function (err, load) {
       if (err) {
         console.log(err);
       }
@@ -51,16 +53,17 @@ module.exports = {
   update: function (req, res) {
     Load.findByIdAndUpdate(req.params.id, {
       name: req.body.name,
-      load: req.body.load,
-      _driver: req.body.driver_id,
-      // _truck: req.body.truck_id,
-      // _trailers: req.body.trailers,
+      number: req.body.number,
+      _drivers: req.body._drivers,
+      _trucks: req.body._trucks,
+      _trailers: req.body._trailers,
       pickup_numbers: req.body.pickup_numbers,
       pickups: req.body.pickups,
       dropoffs: req.body.dropoffs,
       rate_confirmation: req.body.rate_confirmation,
       // Updates will be handled separately by the driver only?
       // updates: req.body.updates,
+      done: req.body.done,
       billed: req.body.billed,
       paid: req.body.paid,
       updated_at: Date.now()
@@ -77,24 +80,18 @@ module.exports = {
       }
       else {
         // if removing the load is successful, findthe drivers this load belongs to
-        User.find({_id: { $in: load._driver } }, function (err, drivers) {
+        User.find({_id: { $in: load._drivers } }, function (err, drivers) {
           if (err) {
             console.log(err);
           }
           else {
             // remove the load from each driver's loads
             for (var i = 0; i < drivers.length; i++) {
-              driver.loads.id(load._id).remove();
-              driver.save( function (err) {
-                if (err) {
-                  console.log(err);
-                }
-                console.log('driver saved');
-              });        
+              drivers[i].loads.id(load._id).remove();
             }
-            // process.nextTick( function () )
-            console.log('sending response');
-            res.end();
+            async.map(drivers, save, function (error) {
+              res.json(load);
+            });
           }
         });
       }
