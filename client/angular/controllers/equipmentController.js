718c4dae4a83e31ad4b547a499c59c77cbf0e8a1
David Jimenez <davidjosuedjdj@gmail.com>
2015-11-30 12:09:44 -0800

    Added Equipment and Drivers

--- client/angular/controllers/equipmentController.js
+++ client/angular/controllers/equipmentController.js
@@ -1,7 +1,18 @@
-fleetLogistics.controller('equipmentController', function($scope, equipmentFactory){
+fleetLogistics.controller('equipmentController', function($scope, $location, equipmentFactory){
 
   equipmentFactory.index( function (data) {
     $scope.equips = data;
+    $scope.trucks = [];
+    $scope.trailers = [];
+    for (var i = 0; i<data.length; i++) {
+        if (data[i].__t == "Truck") {
+            $scope.trucks.push(data[i]);
+        } else {
+            $scope.trailers.push(data[i]);
+        }
+    }
+    console.log("Trucks: ", $scope.trucks);
+    console.log("Trailes: ", $scope.trailers);
   });
 
   $scope.newEquip = {
@@ -11,8 +22,9 @@ fleetLogistics.controller('equipmentController', function($scope, equipmentFacto
   $scope.create = function() {
     // console.log($scope.newEquip);
     equipmentFactory.create($scope.newEquip, function (data) {
-      $scope.equips.push(data);
-      $scope.equip = data;
+    //   $scope.equips.push(data);
+    //   $scope.equip = data;
+     $location.url('/equipment');
     });
   }
 
@@ -21,4 +33,9 @@ fleetLogistics.controller('equipmentController', function($scope, equipmentFacto
       $scope.equip = data;
     })
   }
+
+  $scope.select = function(equipment) {
+      $scope.selected = equipment;
+  }
+
 })
